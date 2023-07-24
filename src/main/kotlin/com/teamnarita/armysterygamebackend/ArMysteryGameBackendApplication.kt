098febf383491e0dbf5b30444f40d490ecf803cd
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-07-24 21:27:49 +0900

    init commit

---
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/ArMysteryGameBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.teamnarita.armysterygamebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArMysteryGameBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArMysteryGameBackendApplication>(*args)
+}
