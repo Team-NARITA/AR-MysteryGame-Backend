ec69307b495993beefee548c8d0133b35216de01
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-07-25 03:03:48 +0900

    GameUserを追加

--- src/main/kotlin/com/teamnarita/armysterygamebackend/model/GameUser.kt
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/model/GameUser.kt
@@ -1,4 +1,34 @@
 package com.teamnarita.armysterygamebackend.model
 
-class GameUser {
+import com.teamnarita.armysterygamebackend.model.dto.UserDTO
+
+class GameUser private constructor(
+    val userId: String,
+    val userName: String,
+    val solvedMystery: HashSet<String>,
+    val clearedChapter: HashSet<String>,
+    val usedCoupon: HashSet<String>
+) {
+    fun addSolvedMystery(mysteryId: String) {
+        solvedMystery.add(mysteryId)
+    }
+
+    fun addClearedChapter(chapterId: String) {
+        clearedChapter.add(chapterId)
+    }
+
+    fun addUsedCoupon(couponId: String) {
+        usedCoupon.add(couponId)
+    }
+
+    class GameUserBuilder(private val userId: String) {
+        lateinit var userName: String
+        lateinit var solvedMystery: HashSet<String>
+        lateinit var clearedChapter: HashSet<String>
+        lateinit var usedCoupon: HashSet<String>
+
+        fun build(): GameUser {
+            return GameUser(userId, userName, solvedMystery, clearedChapter, usedCoupon)
+        }
+    }
 }
