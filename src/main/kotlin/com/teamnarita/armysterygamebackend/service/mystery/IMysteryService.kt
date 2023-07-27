660a9cac33e82a48bd9f0e2d1fb3c9e7ac9a1e0e
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-07-28 01:44:22 +0900

    ミステリーを解いたときのサービスを実装

---
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/service/mystery/IMysteryService.kt
@@ -0,0 +1,8 @@
+package com.teamnarita.armysterygamebackend.service.mystery
+
+import com.teamnarita.armysterygamebackend.model.GameUser
+import com.teamnarita.armysterygamebackend.model.dto.SolvedMystery
+
+interface IMysteryService {
+    fun solveMystery(user: GameUser, mysteryId: String): SolvedMystery
+}
---
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/service/mystery/MysteryService.kt
@@ -0,0 +1,18 @@
+package com.teamnarita.armysterygamebackend.service.mystery
+
+import com.teamnarita.armysterygamebackend.model.GameUser
+import com.teamnarita.armysterygamebackend.model.dto.SolvedMystery
+import com.teamnarita.armysterygamebackend.repository.mystery.IMysteryRepository
+import com.teamnarita.armysterygamebackend.utility.TimeUtil
+import org.springframework.stereotype.Service
+
+@Service
+class MysteryService(private val mysteryRepository: IMysteryRepository): IMysteryService {
+    private val timeUtil = TimeUtil
+
+    override fun solveMystery(user: GameUser, mysteryId: String): SolvedMystery {
+        val solvedMystery = SolvedMystery(user.userId, mysteryId, timeUtil.getCurrentTimeStamp())
+        mysteryRepository.addSolvedMystery(user.userId, solvedMystery)
+        return solvedMystery
+    }
+}
