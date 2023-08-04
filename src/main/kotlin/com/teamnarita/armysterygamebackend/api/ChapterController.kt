e129ef3bca53170c4fd19b2a1d47c9a412e69cb1
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-08-05 01:10:30 +0900

    ファイルのレスポンス追加

---
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/api/ChapterController.kt
@@ -0,0 +1,49 @@
+package com.teamnarita.armysterygamebackend.api
+
+import com.teamnarita.armysterygamebackend.model.ChapterData
+import com.teamnarita.armysterygamebackend.model.UserDetailsImpl
+import com.teamnarita.armysterygamebackend.model.dto.ClearedChapter
+import com.teamnarita.armysterygamebackend.service.chapter.IChapterService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+@RestController
+@RequestMapping("v1/chapter")
+class ChapterController(private val chapterService: IChapterService) {
+    @GetMapping("/file/{chapterId}")
+    fun getChapterFile(@AuthenticationPrincipal principal: UserDetailsImpl, @PathVariable chapterId: String): String {
+        val gameUser = principal.gameUser
+        val chapterFile = chapterService.getChapterFileByUser(gameUser, chapterId)
+
+        val stringBuffer = StringBuffer()
+        val bufferedReader = BufferedReader(InputStreamReader(chapterFile.inputStream(), "UTF-8"))
+
+        stringBuffer.append(bufferedReader.readLines())
+
+        return stringBuffer.toString()
+    }
+
+    @GetMapping("/info/{chapterId}")
+    fun getChapterInfo(@AuthenticationPrincipal principal: UserDetailsImpl, @PathVariable chapterId: String): ResponseEntity<ChapterData> {
+        val gameUser = principal.gameUser
+        val chapterData = chapterService.getChapterDataByUser(gameUser, chapterId)
+        return ResponseEntity(chapterData, HttpStatus.OK)
+    }
+
+    @GetMapping("/currentId")
+    fun getCurrentChapter(@AuthenticationPrincipal principal: UserDetailsImpl): ResponseEntity<ChapterData> {
+        val chapterData = chapterService.getCurrentChapter(principal.gameUser)
+        return ResponseEntity(chapterData, HttpStatus.OK)
+    }
+
+    @PostMapping("/clear/{chapterId}")
+    fun clearChapter(@AuthenticationPrincipal principal: UserDetailsImpl, @PathVariable chapterId: String): ResponseEntity<ClearedChapter> {
+        val gameUser = principal.gameUser
+        val clearedChapter = chapterService.clearChapter(gameUser, chapterId)
+        return ResponseEntity(clearedChapter, HttpStatus.OK)
+    }
+}
