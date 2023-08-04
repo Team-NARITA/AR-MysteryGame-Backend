f4bcba84c71835156c93d415684ee04e9799bf8f
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-08-05 00:05:58 +0900

    チャプター以外にも使えるので場所を移動

--- src/main/kotlin/com/teamnarita/armysterygamebackend/exception/UnauthorizedAccessException.kt
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/exception/UnauthorizedAccessException.kt
@@ -1,4 +1,4 @@
-package com.teamnarita.armysterygamebackend.exception.chapter
+package com.teamnarita.armysterygamebackend.exception
 
 class UnauthorizedAccessException(val userId: String, msg: String): Exception(msg) {
 }
