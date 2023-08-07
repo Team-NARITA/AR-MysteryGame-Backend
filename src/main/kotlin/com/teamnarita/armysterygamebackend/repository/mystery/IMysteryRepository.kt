914b99a56be9df1704fa4c46c927fdd6c5a0dc7b
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-08-07 14:06:33 +0900

    ミステリーのロードを実装

--- src/main/kotlin/com/teamnarita/armysterygamebackend/repository/mystery/IMysteryRepository.kt
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/repository/mystery/IMysteryRepository.kt
@@ -1,8 +1,11 @@
 package com.teamnarita.armysterygamebackend.repository.mystery
 
+import com.teamnarita.armysterygamebackend.model.MysteryData
 import com.teamnarita.armysterygamebackend.model.dto.SolvedMystery
 
 interface IMysteryRepository {
+    fun loadMysteryMaster()
+    fun getMysteryById(mysteryId: String): MysteryData?
     fun getSolvedMystery(userId: String): HashSet<SolvedMystery>
     fun addSolvedMystery(solvedMystery: SolvedMystery)
 }
--- src/main/kotlin/com/teamnarita/armysterygamebackend/repository/mystery/MysteryRepository.kt
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/repository/mystery/MysteryRepository.kt
@@ -1,13 +1,39 @@
 package com.teamnarita.armysterygamebackend.repository.mystery
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.teamnarita.armysterygamebackend.model.MysteryData
 import com.teamnarita.armysterygamebackend.model.dto.SolvedMystery
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.stereotype.Repository
+import java.io.File
+import java.nio.file.Paths
 import java.sql.ResultSet
 
 @Repository
 class MysteryRepository(val jdbcTemplate: JdbcTemplate): IMysteryRepository {
+    companion object {
+        private val mysteryFolder = Paths.get("./mystery/")
+        private val mysteryMaster = File(mysteryFolder.toFile(), "mysteryMaster.json")
+        private val jsonMapper = ObjectMapper().registerKotlinModule()
+    }
+
+    private val mysteryDataMap: HashMap<String, MysteryData> = HashMap()
+
+    override fun loadMysteryMaster() {
+        mysteryDataMap.clear()
+        val mysteries = jsonMapper.readValue<List<MysteryData>>(mysteryMaster)
+        for (mystery in mysteries) {
+            mysteryDataMap[mystery.mysteryId] = mystery
+        }
+    }
+
+    override fun getMysteryById(mysteryId: String): MysteryData? {
+        return mysteryDataMap[mysteryId]
+    }
+
     override fun getSolvedMystery(userId: String): HashSet<SolvedMystery> {
         val solvedMysteries = jdbcTemplate.query("SELECT * FROM solved_mystery WHERE user_id='$userId'", SolvedMysteryRowMapper())
         return solvedMysteries.toHashSet()
