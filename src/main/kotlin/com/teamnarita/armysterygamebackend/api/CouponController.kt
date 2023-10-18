0ce76db133668388eb04517c8418baa54e992e1c
raink1208 <58714901+raink1208@users.noreply.github.com>
2023-10-19 02:21:29 +0900

    クーポンのAPIコントローラーを追加

---
+++ src/main/kotlin/com/teamnarita/armysterygamebackend/api/CouponController.kt
@@ -0,0 +1,43 @@
+package com.teamnarita.armysterygamebackend.api
+
+import com.teamnarita.armysterygamebackend.exception.coupon.CouponAlreadyUsedException
+import com.teamnarita.armysterygamebackend.model.UserCouponData
+import com.teamnarita.armysterygamebackend.model.UserDetailsImpl
+import com.teamnarita.armysterygamebackend.service.coupon.ICouponService
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.ErrorResponse
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("v1/coupon")
+class CouponController(private val couponService: ICouponService) {
+    companion object {
+        private val Logger = LoggerFactory.getLogger(CouponController::class.java)
+    }
+
+    @GetMapping("/")
+    fun getCouponList(@AuthenticationPrincipal principal: UserDetailsImpl): ResponseEntity<List<UserCouponData>> {
+        Logger.info("Request:GET /coupon/ User: ${principal.gameUser.userName}[${principal.gameUser.userId}]")
+        val couponList = couponService.getUserCouponList(principal.gameUser)
+        return ResponseEntity(couponList, HttpStatus.OK)
+    }
+
+    @GetMapping("/use/{couponId}")
+    fun useCoupon(@AuthenticationPrincipal principal: UserDetailsImpl, @PathVariable couponId: String) {
+        Logger.info("Request:GET /coupon/${couponId} User: ${principal.gameUser.userName}[${principal.gameUser.userId}]")
+        TODO("")
+    }
+
+    @ExceptionHandler(CouponAlreadyUsedException::class)
+    fun handleException(ex: CouponAlreadyUsedException): ErrorResponse {
+        return ErrorResponse.create(ex, HttpStatus.FORBIDDEN, "")
+    }
+
+}
